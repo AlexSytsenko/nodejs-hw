0516d619a4bd1b7c71ce9b7605de34e85fbbfb5e
AlexSytsenko <sytsenko@gmail.com>
2021-06-05 23:25:13 +0300

    add functions which work with contacts

--- contacts.js
+++ contacts.js
@@ -1,14 +1,81 @@
 import * as fs from "fs/promises"
 import * as path from "path"
 import { fileURLToPath } from "url";
+import uuidv4 from "./utils/newId.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const contactsPath = path.join(__dirname, "./db/contacts.json")
 
-function logger() {
-  console.log(contactsPath);
+
+
+export async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath)
+    const contactsList = JSON.parse(data);
+
+    console.table(contactsList);
+  } catch (error) {
+    console.log(error.message)
+  }
+
 }
 
-export default logger;
+export async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath)
+    const contactsList = JSON.parse(data)
+    const contact = contactsList.find((contact) => contact.id === contactId)
+
+    if (!contact) {
+      return console.error(`Сontact with id=${contactId} not found`)
+    }
+      console.table(contact)
+
+  } catch (error) {
+     console.log(error.message)
+  }
+}
+
+export async function removeContact(contactId) {
+
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contactsList = JSON.parse(data);
+    const newContactList = contactsList.filter((contact) => contact.id !== contactId);
+
+    if (newContactList.length === contactsList.length) {
+      return console.log(`Сontact with id=${contactId} not found`)
+    }
+
+    fs.writeFile(contactsPath, JSON.stringify(newContactList));
+    console.log(`Сontact with id=${contactId} removed`);
+
+  } catch (error) {
+    console.log(error.message);
+  }
+
+}
+
+
+export async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contactsList = JSON.parse(data);
+
+    const contactInList = contactsList.find(contact => contact.email === email || contact.phone === phone);
+    if (contactInList) {
+      return console.log('Contact already in the list')
+    }
+    const contactNew = { id: uuidv4(), name, email, phone };
+    const newContactsList = JSON.stringify([...contactsList, contactNew]);
+
+    fs.writeFile(contactsPath, newContactsList);
+    
+
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import logger from "./contacts.js";
+import { listContacts, getContactById, removeContact, addContact } from "./contacts.js";
 
-logger();
+
+  // getContactById('6cc3822f-f13d-45c6-a8da-b49b735ef7a9');
+// console.log(uuidv4());
+
+// listContacts()
+
+// addContact("Cyr Ja", "mtts.Cras@non.net", "(740) 222-2688");
